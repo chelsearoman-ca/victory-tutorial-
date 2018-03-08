110c8c3abc42e7e22ae3a67d36749a3417bfb7ec
Chelsea Roman <chelsea.ann.roman@gmail.com>
2018-03-08 09:56:27 -0500

    bar graph

--- src/js/client.js
+++ src/js/client.js
@@ -1,34 +1,130 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryGroup} from 'victory';
 
-const data2012 = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
-
-const data2013 = [
-  {quarter: 1, earnings: 15000},
-  {quarter: 2, earnings: 12500},
-  {quarter: 3, earnings: 19500},
-  {quarter: 4, earnings: 13000}
-];
-
-const data2014 = [
-  {quarter: 1, earnings: 11500},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 20000},
-  {quarter: 4, earnings: 15500}
-];
-
-const data2015 = [
-  {quarter: 1, earnings: 18000},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 15000},
-  {quarter: 4, earnings: 12000}
-];
+// const data = {
+//   "data":{
+//     "venues":[
+//       {
+//         "name": "Punto Gelato",
+//         "totalSalesByDate":[
+//           {
+//             "date": 03092018,
+//             "totalSales": 34
+//           },{
+//             "date": 03082018,
+//             "totalSales": 78
+//           },{
+//             "date": 03072018,
+//             "totalSales": 132
+//           },{
+//             "date": 03062018,
+//             "totalSales": 32
+//           },{
+//             "date": 03052018,
+//             "totalSales": 98
+//           },{
+//             "date": 03042018,
+//             "totalSales": 63
+//           },{
+//             "date": 03032018,
+//             "totalSales": 56
+//           },{
+//             "date": 03022018,
+//             "totalSales": 134
+//           },
+//         ]
+//       },{
+//         "name": "The Grow Op",
+//         "totalSalesByDate":[
+//           {
+//             "date": 03092018,
+//             "totalSales": 163
+//           },{
+//             "date": 03082018,
+//             "totalSales": 87
+//           },{
+//             "date": 03072018,
+//             "totalSales": 112
+//           },{
+//             "date": 03062018,
+//             "totalSales": 133
+//           },{
+//             "date": 03052018,
+//             "totalSales": 129
+//           },{
+//             "date": 03042018,
+//             "totalSales": 240
+//           },{
+//             "date": 03032018,
+//             "totalSales": 217
+//           },{
+//             "date": 03022018,
+//             "totalSales": 122
+//           },
+//         ]
+//       },{
+//         "name": "Rialto Coffee",
+//         "totalSalesByDate":[
+//           {
+//             "date": 03092018,
+//             "totalSales": 200
+//           },{
+//             "date": 03082018,
+//             "totalSales": 89
+//           },{
+//             "date": 03072018,
+//             "totalSales": 54
+//           },{
+//             "date": 03062018,
+//             "totalSales": 78
+//           },{
+//             "date": 03052018,
+//             "totalSales": 186
+//           },{
+//             "date": 03042018,
+//             "totalSales": 210
+//           },{
+//             "date": 03032018,
+//             "totalSales": 211
+//           },{
+//             "date": 03022018,
+//             "totalSales": 76
+//           },
+//         ]
+//       },{
+//         "name": "Glory Hole Donuts",
+//         "totalSalesByDate":[
+          // {
+          //   "date": 03092018,
+          //   "totalSales": 132
+          // },{
+          //   "date": 03082018,
+          //   "totalSales": 109
+          // },{
+          //   "date": 03072018,
+          //   "totalSales": 232
+          // },{
+          //   "date": 03062018,
+          //   "totalSales": 248
+          // },{
+          //   "date": 03052018,
+          //   "totalSales": 89
+          // },{
+          //   "date": 03042018,
+          //   "totalSales": 64
+          // },{
+          //   "date": 03032018,
+          //   "totalSales": 134
+          // },{
+          //   "date": 03022018,
+          //   "totalSales": 170
+          // },
+//         ]
+//       }
+//     ]
+//   }
+// }
 
 class Main extends React.Component {
   render() {
@@ -36,40 +132,73 @@ class Main extends React.Component {
       <div>
         <h1>Victory Tutorial</h1>
         <VictoryChart
-          domainPadding={10}
-          theme={VictoryTheme.material}
-        >
-          <VictoryAxis
-            tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-          />
-          <VictoryAxis
-            dependentAxis
-            tickFormat={(x) => (`$${x / 1000}k`)}
-          />
-          <VictoryStack
-            colorScale={"warm"}
+          // domainPadding will add space to each side of VictoryBar to
+          // prevent it from overlapping the axis
+          height={1000} width={1000}
+          domainPadding={{ x: 10, y: [1, 20] }}
+          scale={{ x: "time" }}
           >
-            <VictoryBar
-              data={data2012}
-              x={"quarter"}
-              y={"earnings"}
+            <VictotyGroup offset={20}
+                  colorScale={"qualitative"}>
+              <VictoryBar
+              data={[
+                {
+                  "date": '03092018',
+                  "totalSales": 132
+                },{
+                  "date": '03082018',
+                  "totalSales": 109
+                },{
+                  "date": '03072018',
+                  "totalSales": 232
+                },{
+                  "date": '03062018',
+                  "totalSales": 248
+                },{
+                  "date":'03052018',
+                  "totalSales": 89
+                },{
+                  "date":'03042018',
+                  "totalSales": 64
+                },{
+                  "date":'03032018',
+                  "totalSales": 134
+                },{
+                  "date": '03022018',
+                  "totalSales": 170
+                }
+              ]} x="date" y="totalSales"
             />
-            <VictoryBar
-              data={data2013}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2014}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2015}
-              x={"quarter"}
-              y={"earnings"}
-            />
-          </VictoryStack>
+            {/* <VictoryBar
+            data={[
+              {
+                "date": '03092018',
+                "totalSales": 132
+              },{
+                "date": '03082018',
+                "totalSales": 109
+              },{
+                "date": '03072018',
+                "totalSales": 232
+              },{
+                "date": '03062018',
+                "totalSales": 248
+              },{
+                "date": '03052018',
+                "totalSales": 89
+              },{
+                "date": '03042018',
+                "totalSales": 64
+              },{
+                "date": '03032018',
+                "totalSales": 134
+              },{
+                "date": '03022018',
+                "totalSales": 170
+              },
+            ]} x="date" y="totalSales"
+          /> */}
+            </VictotyGroup>
         </VictoryChart>
       </div>
     );
---
+++ src/js/data.js
@@ -0,0 +1,125 @@
+const data = {
+  "data":{
+    "venues":[
+      {
+        "name": "Punto Gelato",
+        "totalSalesByDate":[
+          {
+            "date": 03092018,
+            "totalSales": 34
+          },{
+            "date": 03082018,
+            "totalSales": 78
+          },{
+            "date": 03072018,
+            "totalSales": 132
+          },{
+            "date": 03062018,
+            "totalSales": 32
+          },{
+            "date": 03052018,
+            "totalSales": 98
+          },{
+            "date": 03042018,
+            "totalSales": 63
+          },{
+            "date": 03032018,
+            "totalSales": 56
+          },{
+            "date": 03022018,
+            "totalSales": 134
+          },
+        ]
+      },{
+        "name": "The Grow Op",
+        "totalSalesByDate":[
+          {
+            "date": 03092018,
+            "totalSales": 163
+          },{
+            "date": 03082018,
+            "totalSales": 87
+          },{
+            "date": 03072018,
+            "totalSales": 112
+          },{
+            "date": 03062018,
+            "totalSales": 133
+          },{
+            "date": 03052018,
+            "totalSales": 129
+          },{
+            "date": 03042018,
+            "totalSales": 240
+          },{
+            "date": 03032018,
+            "totalSales": 217
+          },{
+            "date": 03022018,
+            "totalSales": 122
+          },
+        ]
+      },{
+        "name": "Rialto Coffee",
+        "totalSalesByDate":[
+          {
+            "date": 03092018,
+            "totalSales": 200
+          },{
+            "date": 03082018,
+            "totalSales": 89
+          },{
+            "date": 03072018,
+            "totalSales": 54
+          },{
+            "date": 03062018,
+            "totalSales": 78
+          },{
+            "date": 03052018,
+            "totalSales": 186
+          },{
+            "date": 03042018,
+            "totalSales": 210
+          },{
+            "date": 03032018,
+            "totalSales": 211
+          },{
+            "date": 03022018,
+            "totalSales": 76
+          },
+        ]
+      },{
+        "name": "Glory Hole Donuts",
+        "totalSalesByDate":[
+          {
+            "date": 03092018,
+            "totalSales": 132
+          },{
+            "date": 03082018,
+            "totalSales": 109
+          },{
+            "date": 03072018,
+            "totalSales": 232
+          },{
+            "date": 03062018,
+            "totalSales": 248
+          },{
+            "date": 03052018,
+            "totalSales": 89
+          },{
+            "date": 03042018,
+            "totalSales": 64
+          },{
+            "date": 03032018,
+            "totalSales": 134
+          },{
+            "date": 03022018,
+            "totalSales": 170
+          },
+        ]
+      }
+    ]
+  }
+}
+
+export default data
